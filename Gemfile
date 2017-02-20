4e4f215cdf27ed66b3487f0023f05c8e018892cf
Ctejeda01 <caro.tejeda01@gmail.com>
2017-02-20 13:30:03 -0600

    Formularios

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'bcrypt'
 gem 'rails', '~> 5.0.1'
 gem 'devise'
 # Use sqlite3 as the database for Active Record
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [¨:index, :show ]
 
   # GET /pins
   # GET /pins.json
@@ -69,6 +70,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:photo, :description)
+      params.require(:pin).permit(:photo, :description, :name)
     end
 end
